7979525b111a9518b353c68cc781401645b05f46
tedraykov <tedraykov@gmail.com>
2023-01-27 16:28:15 +0200

    feat: under construction page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  const showDot = [false, false, false];
+  let intervalId;
+
+    const animateDots = () => {
+      intervalId = setInterval(() => {
+        if (!showDot[0]) showDot[0] = true;
+        else if (!showDot[1]) showDot[1] = true;
+        else if (!showDot[2]) showDot[2] = true;
+        else {
+          resetAnimation();
+        }
+      }, 200);
+    }
+
+    const resetAnimation = () => {
+      clearInterval(intervalId);
+      setTimeout(() => {
+        showDot[0] = false;
+        showDot[1] = false;
+        showDot[2] = false;
+        animateDots();
+      }, 500);
+    }
+
+    animateDots();
+</script>
+
+<main class="bg-gray-900 max-w-7xl mx-auto flex-1 flex items-center">
+    <header class="w-full flex justify-center">
+        <h1 class="text-3xl text-gray-400">The site is under construction
+            <span class="dot {showDot[0] && 'visible'}">.</span >
+            <span class="dot {showDot[1] && 'visible'}">.</span>
+            <span class="dot {showDot[2] && 'visible'}">.</span>
+        </h1>
+    </header>
+</main>
+<footer class="max-w-7xl mx-auto py-2">
+    <p class="text-gray-600">Tech TPD. Copyrights {new Date().getFullYear()}</p>
+</footer>
+
+<style>
+    .dot {
+        @apply opacity-0 transition-opacity duration-300;
+    }
+    .visible {
+        @apply opacity-100;
+    }
+</style>
